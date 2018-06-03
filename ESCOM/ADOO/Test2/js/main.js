86c63cf73ab1f77347bfa0a562431090ab1437bd
Manuel CH <manuel_7795@hotmail.com>
2018-06-03 16:27:45 -0500

    a

--- ESCOM/ADOO/Test2/js/main.js
+++ ESCOM/ADOO/Test2/js/main.js
@@ -124,94 +124,113 @@ function drawPieChart() {
 }
 
 
-/*
-filedrag.js - HTML5 File Drag & Drop demonstration
-Featured on SitePoint.com
-Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
-*/
-(function() {
 
-	// getElementById
-	function $id(id) {
-		return document.getElementById(id);
-	}
+// ************************ Drag and drop ***************** //
+function startRead(evt) {  
+  var file = document.getElementById('file').files[0];
+  if(file){
+    if(file.type.match("image.*"))
+    {
+    getAsImage(file);
+    alert("Name: "+file.name +"\n"+"Last Modified Date :"+file.lastModifiedDate);
+    }
+    else
+    {
+    getAsText(file);
+    alert("Name: "+file.name +"\n"+"Last Modified Date :"+file.lastModifiedDate);
+    }
+    }
+    evt.stopPropagation();
+    evt.preventDefault();
+}
 
-
-	// output information
-	function Output(msg) {
-		var m = $id("messages");
-		m.innerHTML = msg + m.innerHTML;
-	}
-
-
-	// file drag hover
-	function FileDragHover(e) {
-		e.stopPropagation();
-		e.preventDefault();
-		e.target.className = (e.type == "dragover" ? "hover" : "");
-	}
-
-
-	// file selection
-	function FileSelectHandler(e) {
-
-		// cancel event and hover styling
-		FileDragHover(e);
-
-		// fetch FileList object
-		var files = e.target.files || e.dataTransfer.files;
-
-		// process all File objects
-		for (var i = 0, f; f = files[i]; i++) {
-			ParseFile(f);
-		}
-
-	}
+function startReadFromDrag(evt) {
+    var file = evt.dataTransfer.files[0];
+    if (file) {
+        if (file.type.match("image.*")) {
+            getAsImage(file);
+            alert("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
+        }
+        else {
+            getAsText(file);
+            alert("Name: " + file.name + "\n" + "Last Modified Date :" + file.lastModifiedDate);
+        }
+    }
+    evt.stopPropagation();
+    evt.preventDefault();
+}
 
 
-	// output file information
-	function ParseFile(file) {
+function getAsText(readFile) {
+        
+  var reader = new FileReader();
+  
+  // Read file into memory as UTF-16      
+  reader.readAsText(readFile, "UTF-8");
+  
+  // Handle progress, success, and errors
+  reader.onprogress = updateProgress;
+  reader.onload = loaded;
+  reader.onerror = errorHandler;
+}
 
-		Output(
-			"<p>File information: <strong>" + file.name +
-			"</strong> type: <strong>" + file.type +
-			"</strong> size: <strong>" + file.size +
-			"</strong> bytes</p>"
-		);
+function getAsImage(readFile) {
+        
+  var reader = new FileReader();
+  
+  // Read file into memory as UTF-16      
+  reader.readAsDataURL(readFile);
+  
+  // Handle progress, success, and errors
+  reader.onload = addImg;
+}
 
-	}
+function updateProgress(evt) {
+  if (evt.lengthComputable) {
+    // evt.loaded and evt.total are ProgressEvent properties
+    var loaded = (evt.loaded / evt.total);
+    
+    if (loaded < 1) {
+      // Increase the prog bar length
+      // style.width = (loaded * 200) + "px";
+    }
+  }
+}
 
+function loaded(evt) {  
+  // Obtain the read file data    
+  var fileString = evt.target.result;
+  $("#op").text(fileString);
+  alert("file Loaded successfully");
+  // Handle UTF-16 file dump
+  if(utils.regexp.isChinese(fileString)) {
+    //Chinese Characters + Name validation
+  }
+  else {
+    // run other charset test
+  }
+  // xhr.send(fileString)     
+}
 
-	// initialize
-	function Init() {
+function errorHandler(evt) {
+  if(evt.target.error.name == "NotReadableError") {
+    // The file could not be read
+  }
+}
+function domagic(evt){
+$("#draghere").css("background-color","green");
+  evt.stopPropagation();
+    evt.preventDefault();  
+}
 
-		var fileselect = $id("fileselect"),
-			filedrag = $id("filedrag"),
-			submitbutton = $id("submitbutton");
+function addImg(imgsrc){
+ var img = document.createElement('img');
+  img.setAttribute("src",imgsrc.target.result);
+  img.setAttribute("height","100");
+  img.setAttribute("width","300");
+  document.getElementById("op").insertBefore(img);
+}
 
-		// file select
-		fileselect.addEventListener("change", FileSelectHandler, false);
-
-		// is XHR2 available?
-		var xhr = new XMLHttpRequest();
-		if (xhr.upload) {
-
-			// file drop
-			filedrag.addEventListener("dragover", FileDragHover, false);
-			filedrag.addEventListener("dragleave", FileDragHover, false);
-			filedrag.addEventListener("drop", FileSelectHandler, false);
-			filedrag.style.display = "block";
-
-			// remove submit button
-			submitbutton.style.display = "none";
-		}
-
-	}
-
-	// call initialization file
-	if (window.File && window.FileList && window.FileReader) {
-		Init();
-	}
-
-
-})();
+  var dropingDiv = document.getElementById('draghere');
+  dropingDiv.addEventListener('dragover', domagic, false);
+  dropingDiv.addEventListener('drop', startReadFromDrag, false);
